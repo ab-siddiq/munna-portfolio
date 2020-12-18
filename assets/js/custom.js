a797e7c499ea7a1d89854abb5a1b37f4757b495c
siddiq <siddiq.aiub@gmail.com>
2020-12-18 12:11:49 +0600

    update

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -137,6 +137,43 @@
 			stop_videos();
 		}
 	});
+    
+    
+    //slick slider team
+$('.autoplay-youtube').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+    
+     responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    
+  ],
+   
+});
 
 
 })(jQuery);
